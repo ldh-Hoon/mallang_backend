cab38f765376bf7ca91d50f40553d696e7f991f1
Hoon <a32252613@gmail.com>
2024-02-23 17:06:44 +0900

    .

--- utils/data_control.py
+++ utils/data_control.py
@@ -81,8 +81,6 @@ def login_check(email, password): # 로그인 검증
 def add_account(email, password, name): #  계정 추가
     data = get_json()
 
-    if email in data:
-        return False
     data[email]={
             "pw" : password,
             "info" : {
