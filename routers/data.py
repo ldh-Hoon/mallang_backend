470e3f1d21b280041237db574b47803a37179fe5
Hoon <a32252613@gmail.com>
2024-02-25 14:40:25 +0900

    .

--- routers/data.py
+++ routers/data.py
@@ -1,11 +1,11 @@
-from fastapi import APIRouter, UploadFile, BackgroundTasks
+from fastapi import APIRouter, UploadFile
 from pydantic import BaseModel
 from typing import Optional
 from fastapi.responses import JSONResponse, FileResponse
 
 from utils.convert import *
 from utils.data_control import *
-import os, requests
+import os
 from utils.urls import *
 
 
@@ -17,30 +17,13 @@ class File_request_payload(BaseModel):
     book: Optional[str] = None
     file: Optional[str] = None
 
-def tts_save(book_data, file):
-    with open(file, 'rb') as f:
-        raw = f.read()
-    for scene in book_data['script']:
-        if scene['role']=='나레이션':
-            files = {'wav': raw}
-            d = {'text': scene['text']}
-            res = requests.post(TTS_ENDPOINT, files=files, data=d)
-            with open(f'books/{book_data["title"]}/voices/{scene["id"]}.mp3', 'wb') as file:
-                file.write(res.content)
 
 @data_api.post("/upload/parent_audio/{email}")
-async def upload_audio(file : UploadFile, email, background_tasks: BackgroundTasks):
+async def upload_audio(file : UploadFile, email):
     content = await file.read()
     with open(f"parent/{clean_text(email)}.aac", 'wb') as file:
         file.write(content)
     convert_aac2wav(f"parent/{clean_text(email)}")
-
-    file = f"parent/a1.wav"
-    if os.path.isfile(f"parent/{clean_text(email)}.wav"):
-        file = f"parent/{clean_text(email)}.wav"
-    book_data = book_json("토끼와 거북이")
-    
-    background_tasks.add_task(tts_save, book_data, file)
     return "ok"
 
 @data_api.get('/{type}/{book}/{filename}')
