f803af5c0f2802170c4516cb97ec4620568fa62f
ldh-Hoon <139981434+ldh-Hoon@users.noreply.github.com>
2024-05-19 21:36:20 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,19 @@ from fastapi.responses import JSONResponse
 from routers.api import api
 from routers.account import account
 from routers.data import data_api
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 특정 출처만 허용하려면 출처의 리스트를 사용하세요.
+    allow_credentials=True,
+    allow_methods=["*"],  # 특정 HTTP 메소드만 허용하려면 메소드의 리스트를 사용하세요.
+    allow_headers=["*"],
+)
 
 app.include_router(api)
 app.include_router(account)
